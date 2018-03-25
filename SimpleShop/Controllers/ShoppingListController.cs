3131af8223c6d21120640f59357dca8b4b96062d
Daron Short <daron.short@hotmail.com>
2018-03-25 09:18:54 -0500

    Added ShoppingList Controller and dummy Lists.

---
+++ SimpleShop/Controllers/ShoppingListController.cs
@@ -0,0 +1,46 @@
+﻿using SimpleShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleShop.Controllers
+{
+    public class ShoppingListController : ApiController
+    {
+        List<ShoppingList> shoppingLists = new List<ShoppingList> {
+            new ShoppingList() { Id=0, Name = "Groceries"},
+            new ShoppingList() { Id=1, Name = "Hardware"}
+        };
+
+
+        // GET: api/ShoppingList
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/ShoppingList/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ShoppingList
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/ShoppingList/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ShoppingList/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
